08a3d97fdc4526a0b970c6cfa98578487194ba73
Erin K <erin@penguinstampede.com>
2017-03-26 14:34:08 -0700

    getting the owner name for the first load

--- hackagist.js
+++ hackagist.js
@@ -18,7 +18,7 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
 
-  let hackaday_projects = {
+  var hackaday_projects = {
     uri: 'https://api.hackaday.io/v1/projects',
     qs: {
         api_key: hdapi
@@ -27,15 +27,46 @@ router.get('/', function(req, res) {
         'User-Agent': 'Request-Promise'
     },
     json: true // Automatically parses the JSON string in the response
-  };
+  },
+  the_projects = {};
+
+
+  function get_owners(projects){
+    var hackaday_owners = {
+      qs: {
+          api_key: hdapi
+      },
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true // Automatically parses the JSON string in the response
+    }, promises = [];
+    for (p in projects) {
+      hackaday_owners.uri = 'https://api.hackaday.io/v1/users/' + projects[p].owner_id;
+      promises.push(rp(hackaday_owners));
+    }
+
+    Promise.all(promises)
+      .then((results) => {
+        res.render('index', {the_projects: the_projects, the_owners: results});
+      })
+      .catch((e) => {
+          // Handle errors here
+      });
+
+  }
+
+  //now we really start. get the project list and move through it.
 
   rp(hackaday_projects)
     .then(function (response) {
-      res.render('index', {project_query: response});
+      the_projects = response;
+      get_owners(the_projects.projects);
     })
     .catch(function (err) {
       console.log(err);
     });
+
 });
 
 hackagist.use('/', router);
--- test/index.js
+++ test/index.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 process.env.NODE_ENV = 'test';
 
 //I'm connecting to SauceLabs with envionrment vars for Selenium
-let assert = require('assert'),
-  webdriver = require('selenium-webdriver'),
-  username    = process.env.SAUCE_USERNAME,
-  accessKey   = process.env.SAUCE_ACCESS_KEY,
-  driver;
+const assert    = require('assert'),
+  webdriver     = require('selenium-webdriver'),
+  username      = process.env.SAUCE_USERNAME,
+  accessKey     = process.env.SAUCE_ACCESS_KEY;
+  chai          = require('chai');
+  server        = require('../hackagist');
+  chaiWebdriver = require('chai-webdriver');
 
-driver = new webdriver.Builder().
+let driver = new webdriver.Builder().
   withCapabilities({
     'browserName': 'chrome',
     'platform': 'Windows XP',
@@ -21,15 +23,12 @@ driver = new webdriver.Builder().
               "@localhost:4445/wd/hub").
   build();
 
-const chai = require('chai');
-const server = require('../hackagist');
-const chaiWebdriver = require('chai-webdriver');
-
-chai.use(chaiWebdriver(driver));
-
+//since we're testing locally
 let url = function(page){
   return "http://127.0.0.1:8080" + page;
 }
+
+chai.use(chaiWebdriver(driver));
 
 after( (done) => {
   driver.quit()
@@ -38,15 +37,28 @@ after( (done) => {
 
 describe('index page', () => {
 
+  //load the page first
   before( function (done) {
     this.timeout(0);
     driver.get(url('/'))
       .then( () => { done(); });
   });
 
-  describe('load index', () => {
+  describe('Render a page that shows a list or grid of projects (using GET /projects).', () => {
     it('should provide 50 projects', (done) => {
-      chai.expect('li').dom.to.have.count(50);
+      chai.expect('article.project').dom.to.have.count(50);
+      done();
+    });
+    it('should have an owner name for every project', (done) => {
+      chai.expect('article.project h2.owner:empty').dom.to.have.count(0);
+      done();
+    });
+    it('should have an owner id stored in data-owner-id for every project', (done) => {
+      chai.expect('article.project h2.owner').dom.to.have.attribute('data-owner-id');
+      done();
+    });
+    it('should not have an empty list of tags', (done) => {
+      chai.expect('article.project .tags:empty').dom.to.have.count(0);
       done();
     });
   });

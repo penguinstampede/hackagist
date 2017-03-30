7567b41d11f68868d76fecc6fbd843b7a7881edc
Erin K <erin@penguinstampede.com>
2017-03-30 00:29:04 -0700

    big dumb bug fix

--- grunt/config/uglify.js
+++ grunt/config/uglify.js
@@ -12,7 +12,7 @@ module.exports = {
   dist: {
     files: {
       'public/assets/js/vendor.js': ['node_modules/ejs/ejs.js','node_modules/numeral/numeral.js','node_modules/moment/moment.js','assets/bower/masonry/dist/masonry.pkgd.js','assets/bower/imagesloaded/imagesloaded.pkgd.js'],
-      'public/assets/js/app.js': ['assets/js/app.js']
+      'public/assets/js/app.js': ['assets/js/projects.js','assets/js/owners.js','assets/js/tooltip.js','assets/js/app.js']
     },
     options:{
       sourceMap: false,
